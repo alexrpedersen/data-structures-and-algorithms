41b3d293ec30eb8fb08e569095dbc6cbf754c74c
alexrpedersen <alex.r.pedersen@outlook.com>
2020-05-08 16:17:29 -0700

    [assing test

--- arrayBinarySearch/__test__/array-binary-search.test.js
+++ arrayBinarySearch/__test__/array-binary-search.test.js
@@ -5,18 +5,18 @@ const insertBinarySearch = require('../array-binary-search.js');
 describe ('array-binary-search', () => {
 
      it ('returns null if no args are present', () => {
-         expects(insertBinarySearch()).toBeNull()
+         expect(insertBinarySearch()).toBeNull()
     })
 
      it ('recognizes a invalid input', () => {
-        expects(true).toBeTruthy()
+        expect(true).toBeTruthy()
     })
 
      it ('result when array is present', () => {
-        expects(insertBinarySearch([4,8,15,16,23,42], 15)).toEqual(2)
+        expect(insertBinarySearch([4,8,15,16,23,42], 15)).toEqual(2)
     })
 
      it ('result when array is not present', () => {
-        expects(insertBinarySearch([11,22,33,44,55,66,77], 90)).toEqual(-1)
+        expect(insertBinarySearch([11,22,33,44,55,66,77], 90)).toEqual(-1)
     })
 });
